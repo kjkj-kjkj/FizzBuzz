7fa648f2a54ac27cedc0421f23a8b1b8cbcb7c26
kjkj-kjkj <jasmeetkh01@gmail.com>
2022-05-08 17:42:16 +0100

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,16 @@
 
 // More Information about this assisngment is available in the README.md file
 
+for (let i = 1; i < 101; i++) {
+    if (i % 3 == 0 && i % 5== 0){
+        output ("FizzBuzz")
+    }
+    else if (i % 3 == 0){
+        output ("Fizz"); }
+    else if (i % 5 == 0){
+        output ("Buzz")
+    }
+    else {
+        output(i);
+    }
+}
